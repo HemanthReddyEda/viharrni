dc0b8bc7fae36fedfe052de2f9e5cfc2df8ff0bc
HemanthReddyEda <ehreddy14@gmail.com>
2024-11-08 05:33:08 +0530

    2nd commit

--- HOM-Frontend/vite.config.js
+++ HOM-Frontend/vite.config.js
@@ -1,7 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  resolve: {
+    alias: {
+      lodash: 'lodash-es',  // Optional: use the ES module version of lodash
+    },
+  },
+  build: {
+    rollupOptions: {
+      external: ['lodash'],  // Optional: try adding lodash as external if you want to exclude it from the build
+    },
+  },
+});
